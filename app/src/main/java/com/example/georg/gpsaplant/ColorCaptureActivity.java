50b54ead60c06191e6da92d6a3fc44c7e8924acf
eldok11 <greko12345@hotmail.com>
2017-12-19 14:29:03 +0100

    Step 8 Implicit Intent, open gallery

--- app/src/main/java/com/example/georg/gpsaplant/ColorCaptureActivity.java
+++ app/src/main/java/com/example/georg/gpsaplant/ColorCaptureActivity.java
@@ -1,6 +1,9 @@
 package com.example.georg.gpsaplant;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,7 +12,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import java.io.File;
+
 public class ColorCaptureActivity extends PlantPlaecesActivity {
+
+    public static final int IMAGE_GALLERY_REQUEST = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +32,19 @@ public class ColorCaptureActivity extends PlantPlaecesActivity {
     @Override
     public int getCurrentMenuId() {
         return R.id.capturecolor;
+    }
+
+    public void onImageGalleryClicked(View view) {
+        //invoke image gallery with implicit intent, parameter, I want to pick something
+        Intent photoPickerIntent=new Intent(Intent.ACTION_PICK);
+        //where to find that
+        File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        String pictureDirectoryPath = pictureDirectory.getPath();
+        //finally get URI
+        Uri data= Uri.parse(pictureDirectoryPath);
+        //set the data and type, get all imagetypes
+        photoPickerIntent.setDataAndType(data,"image/*");
+        startActivityForResult(photoPickerIntent, IMAGE_GALLERY_REQUEST);
+
     }
 }
