2bee5ce77bf17f896838649357053ecb672f0fa0
eldok11 <greko12345@hotmail.com>
2017-12-19 15:02:54 +0100

    Step 9 Get Image and sho in imageView

--- app/src/main/java/com/example/georg/gpsaplant/ColorCaptureActivity.java
+++ app/src/main/java/com/example/georg/gpsaplant/ColorCaptureActivity.java
@@ -1,9 +1,12 @@
 package com.example.georg.gpsaplant;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.renderscript.ScriptGroup;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,12 +14,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class ColorCaptureActivity extends PlantPlaecesActivity {
 
     public static final int IMAGE_GALLERY_REQUEST = 20;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +32,8 @@ public class ColorCaptureActivity extends PlantPlaecesActivity {
         setContentView(R.layout.activity_color_capture);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        imageView = findViewById(R.id.imageView);
 
 
     }
@@ -42,9 +52,36 @@ public class ColorCaptureActivity extends PlantPlaecesActivity {
         String pictureDirectoryPath = pictureDirectory.getPath();
         //finally get URI
         Uri data= Uri.parse(pictureDirectoryPath);
-        //set the data and type, get all imagetypes
+        //set the data and type, wo und welche daten bei starten des intents sollen angezeigt werden.
+        //setdata wird meistens dazu verwernde um auf etwas zu zeigen
         photoPickerIntent.setDataAndType(data,"image/*");
+
         startActivityForResult(photoPickerIntent, IMAGE_GALLERY_REQUEST);
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode==RESULT_OK){
+            if(requestCode==IMAGE_GALLERY_REQUEST){
+                //adress of picked image
+                Uri imageUri=data.getData();
+                //read image data from sd card.
+                InputStream inputStream;
+                try {
+                    inputStream=getContentResolver().openInputStream(imageUri);
+                    //get bitman from the stream
+                    Bitmap image = BitmapFactory.decodeStream(inputStream);
+                    imageView.setImageBitmap(image);
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    Toast.makeText(this,"Fehler beim streamen",Toast.LENGTH_LONG).show();
+                }
+                //Bitmap cameraImage = (Bitmap)data.getExtras().get("data");
+                //imageView.setImageBitmap(cameraImage);
+
+            }
+        }
     }
 }
