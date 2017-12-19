dd7cb893aaed585f36d99064acfc5019327f786c
eldok11 <greko12345@hotmail.com>
2017-12-19 01:00:36 +0100

    Step 2 Take Photo and show in ImageView

--- app/src/main/java/com/example/georg/gpsaplant/GPSAplant.java
+++ app/src/main/java/com/example/georg/gpsaplant/GPSAplant.java
@@ -1,6 +1,9 @@
 package com.example.georg.gpsaplant;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,11 +12,14 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AutoCompleteTextView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class GPSAplant extends AppCompatActivity {
 
+    public static final int CAMERA_REQUEST = 10;
     private AutoCompleteTextView actPlantName;
+    private ImageView imgSpecimenPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,7 @@ public class GPSAplant extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         actPlantName = findViewById(R.id.actPlantName);
+        imgSpecimenPhoto = findViewById(R.id.imageSpecimenPhoto);
 
 
     }
@@ -55,5 +62,25 @@ public class GPSAplant extends AppCompatActivity {
     public void btnShowSavedClicked(View v){
        String plantName= actPlantName.getText().toString();
        Toast.makeText(this,plantName,Toast.LENGTH_LONG).show();
+    }
+
+    public void btnTakePhotoClicked(View view) {
+        Intent cameraIntent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+        
+    }
+    //wenn wir staractivityforresult aufrufen, brauchen wir onactivityResult
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==RESULT_OK){
+            if(requestCode==CAMERA_REQUEST){
+                Bitmap cameraImage = (Bitmap)data.getExtras().get("data");
+                imgSpecimenPhoto.setImageBitmap(cameraImage);
+
+
+            }
+        }
     }
 }
